ec95b1dbe59e122aad935eda0ebdf0879d90e5be
XXL <xurishi@gmail.com>
2024-09-24 10:10:04 +0800

    build d3

---
+++ rollup.config.mjs
@@ -0,0 +1,10 @@
+import resolve from '@rollup/plugin-node-resolve'
+
+export default {
+    input: 'src/main.js',
+    output: {
+        file: 'bundle.js',
+        format: 'iife'
+    },
+    plugins: [resolve()]
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import * as d3 from 'd3';
+
+export default function () {
+    const forceSim = d3.forceSimulation();
+    return forceSim;
+}
